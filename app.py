bf1123e6a276c45e75c7f4bcd3b085c2dc4df1a6
Aayush Narayan <140238756+Aayush-Narayan@users.noreply.github.com>
2024-09-02 14:56:54 +0000

    Created the website frame using HTML and CSS.

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def hello_world():
-  return "<p>Hello, Aayush!</p>"
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
-
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
